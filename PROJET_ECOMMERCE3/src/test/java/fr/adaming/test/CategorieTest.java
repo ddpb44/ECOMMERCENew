66dbb87f028cf6ce23087ecb2cae75662bc31cdc
inti0243 <inti0243@192.168.1.166>
2017-11-15 16:47:34 +0100

    Kikky --> test unitaire getAllCategorie

--- PROJET_ECOMMERCE3/src/test/java/fr/adaming/test/CategorieTest.java
+++ PROJET_ECOMMERCE3/src/test/java/fr/adaming/test/CategorieTest.java
@@ -2,21 +2,30 @@ package fr.adaming.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import fr.adaming.model.Categorie;
 import fr.adaming.service.ICategorieService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/application-context.xml" })
 public class CategorieTest {
-	
+
 	// ==================== 1 =====================
 	// Injection des collaborateurs
 	// ============================================
+	@Autowired
 	private ICategorieService catService;
 
 	// ==================== 2 =====================
@@ -32,23 +41,23 @@ public class CategorieTest {
 
 	// ============== TODO Test de getAllCategorie
 	// Taille de la liste
-	@Ignore
+	//@Ignore
 	@Test
 	public void testGetAllCategorieTaille() {
 
 		System.out.println("---------- Tester la méthode getAllCategorieTaille");
-		assertEquals(3, (catService.getAllCategorie().size()));
+		List<Categorie> liste = catService.getAllCategorie();
+		assertEquals(3, liste.size());
 
 	}
-	
+
 	// Premier élément de la liste
-	@Ignore
+	//@Ignore
 	@Test
 	public void testGetAllCategorieFirst() {
-		
-		System.out.println("---------- Tester la méthode getAllCategorieFirst");
-		assertEquals(new String("Ondes positivies"), catService.getAllCategorie().get(0).getNomCategorie());
-		
-	}
 
+		System.out.println("---------- Tester la méthode getAllCategorieFirst");
+		assertEquals(new String("Ondes positives"), catService.getAllCategorie().get(0).getNomCategorie());
+
+	}
 }
