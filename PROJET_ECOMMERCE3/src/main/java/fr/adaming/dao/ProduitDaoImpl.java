c59044fa667db307dd0ac33d46e78560f221c564
inti0455 <inti0455@192.168.1.181>
2017-11-16 10:44:38 +0100

    Denis : commit javadoc pdroduitdao

--- PROJET_ECOMMERCE3/src/main/java/fr/adaming/dao/ProduitDaoImpl.java
+++ PROJET_ECOMMERCE3/src/main/java/fr/adaming/dao/ProduitDaoImpl.java
@@ -14,71 +14,125 @@ import fr.adaming.model.Produit;
 
 @Repository
 public class ProduitDaoImpl implements IProduitDao {
-	
+
+	// ==================== 1 =====================
+	// Injection automatique du sessionFactoryBean
+	// ============================================
 	@Autowired
 	private SessionFactory sf;
 
+	// ==================== 2 =====================
+	// Setter pour l'injection
+	// ============================================
 	public void setSf(SessionFactory sf) {
 		this.sf = sf;
 	}
 
+	// ==================== 3 =====================
+	// Méthodes
+	// ============================================
+
+	/**
+	 * This method recovers the entire Produit list in the database
+	 * 
+	 * @return A list with all the Produits in the database
+	 */
 	@Override
 	public List<Produit> getAllProduits() {
 
 		Session s = sf.getCurrentSession();
-		
+
 		String req = "select * from Produit p";
-		
+
 		Query query = s.createQuery(req);
-		
+
 		@SuppressWarnings("unchecked")
 		List<Produit> listeProduits = query.list();
-		
+
 		return listeProduits;
 	}
 
+	/**
+	 * This method recovers a Produit by this id in the database
+	 * 
+	 * @param produit
+	 *            The produit to add
+	 * @return A single Produit in the database
+	 */
 	@Override
 	public Produit getProduitById(Produit produit) {
 		Session session = sf.getCurrentSession();
 		return (Produit) session.get(Produit.class, produit.getId_produit());
 	}
-	
+
+	/**
+	 * This method recovers a Produit by this designation in the database
+	 * 
+	 * @param produit
+	 *            The produit to add
+	 * 
+	 * @return A single Produit in the database
+	 */
 	@Override
 	public Produit getProduitByDes(Produit produit) {
 		Session session = sf.getCurrentSession();
 		return (Produit) session.get(Produit.class, produit.getId_produit());
 	}
 
+	/**
+	 * Add a Produit to the existing list of Produits
+	 * 
+	 * @param produit
+	 *            The produit to add
+	 * 
+	 * @return The added Produit
+	 */
 	@Override
 	public Produit addProduit(Produit produit) {
 		Session session = sf.getCurrentSession();
-		
+
 		session.save(produit);
-		
+
 		return produit;
 	}
 
+	/**
+	 * Update a Produit and modify the list of Produits
+	 * 
+	 * @param produit
+	 *            The Produit to update
+	 * 
+	 * @return The updated Produit
+	 */
 	@Override
 	public Produit updateProduit(Produit produit) {
 		Session session = sf.getCurrentSession();
-		
+
 		session.update(produit);
-		
+
 		return produit;
 	}
 
+	/**
+	 * Delete a Produit from the existing list of Produits
+	 * 
+	 * @param produit
+	 *            The Produit to delete
+	 * 
+	 * @return The deleted Produit
+	 */
 	@Override
 	public int deleteProduit(Produit produit) {
 		Session session = sf.getCurrentSession();
-		
+
 		String req = "delete from Produit p where p.id_produit=:pId";
-		
+
 		Query query = session.createQuery(req);
-		
+
 		query.setParameter("pId", produit.getId_produit());
-		
+
 		int verif = query.executeUpdate();
-		
+
 		return verif;
 	}
 
