fcabfbb59b2ca1aa054fe7a6941eb1af392ca798
inti0243 <inti0243@DESKTOP-UC6DR3I>
2017-11-18 20:23:12 +0100

    kikky --> commandcontroller

--- PROJET_ECOMMERCE3/src/main/java/fr/adaming/controllers/CommandeController.java
+++ PROJET_ECOMMERCE3/src/main/java/fr/adaming/controllers/CommandeController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import fr.adaming.service.IClientService;
 import fr.adaming.service.ICommandeService;
 
 @Controller
