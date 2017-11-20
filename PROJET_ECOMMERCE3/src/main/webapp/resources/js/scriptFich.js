018634ba05996aaa09adadfd81a6efafd6c1eea0
inti-0265 <inti-0265@192.168.1.170>
2017-11-20 11:29:21 +0100

    Anissa :pdf marche avec le style aussi et image

--- PROJET_ECOMMERCE3/src/main/webapp/resources/js/scriptFich.js
+++ PROJET_ECOMMERCE3/src/main/webapp/resources/js/scriptFich.js
@@ -1,21 +1,34 @@
 $(document).ready(function() {
-			$("#btnPrint").on("click",function() {
-				var conceptStore = $("#dvContainer").html();
-				var printWindow = window.open('','_blank','height=400,width=800');
-				//ecrire le html dans la nouvelle fenêtre
-				printWindow.document.write('<html><head><title>ConceptStore</title>');
-//			    printWindow.document.write('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" absolute = true>');
+	$("#btnPrint").on("click", function() {
+		var conceptStore = $("#dvContainer").html();
+		var printWindow = window.open('', '_blank', 'height=400,width=800');
+		var is_chrome = Boolean(printWindow.chrome);
+		// ecrire le html dans la nouvelle fenêtre
+		printWindow.document.write('<html><head><title>ConceptStore</title>');
+		// printWindow.document.write('<link rel="stylesheet"
+		// href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+		// absolute = true>');
 
-				printWindow.document.write('</head><body>');
-				
-				 // Add the stylesheet link and inline styles to the new document:
-//			    printWindow.document.write('<link rel="stylesheet" href="resources/css/styleFich.css">');
-			    
-				printWindow.document.write(conceptStore);
-				printWindow.document.write('</body></html>');
+		printWindow.document.write('</head><body>');
+
+		// Add the stylesheet link and inline styles to the new document:
+		 printWindow.document.write('<link rel="stylesheet" href="resources/css/styleFich.css">');
+
+		printWindow.document.write(conceptStore);
+		printWindow.document.write('</body></html>');
+
+		if (is_chrome) {
+			setTimeout(function() { // wait until all resources loaded
 				printWindow.document.close();
-				printWindow.focus(); 
+				printWindow.focus();
 				printWindow.print();
-				 setTimeout(function(){ printWindow.print(); printWindow.close();}, 990000000000);
-			});
+				printWindow.close();
+			}, 250);
+		} else {
+			printWindow.document.close();
+			printWindow.focus();
+			printWindow.print();
+			printWindow.close();
+		}
+	});
 });
